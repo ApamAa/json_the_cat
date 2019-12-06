e92d283e8849e13de4c3d2b66a4a9ab4092a57a1
ApamAa <apama.jamshidi@gmail.com>
2019-12-06 02:01:29 +0000

    breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,20 @@
-const request = require('request');
-const catbreed = process.argv[2];
-const url = 'http://api.thecatapi.com/v1/breeds/search?q=' + catbreed;
 
-request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  
-  const data = JSON.parse(body);
-  if (data.length !== 0) {
-    console.log(data[0].description);
-  } else {
-    console.log('This breed is not available');
-  }
-}
-);
+const request = require('request');
+const fetchBreedDescription = function(breedName,callbackfunction) {
+  let url = 'http://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    //console.log(body);
+    const data = JSON.parse(body);
+    if (error === null && data.length !== 0) {
+      callbackfunction(null,(data[0].description));
+    } else {
+      callbackfunction(true,null);
+    }
+                
+  });
+            
+};
+
+module.exports = {fetchBreedDescription};
+
+
